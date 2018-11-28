7ca12e25dc7462582a232ae912f83d3ddc0dc67f
linlinlin <linlinlin@172.20.95.229>
2018-11-28 17:50:40 +0100

    ok

--- src/main/java/SoapClient.java
+++ src/main/java/SoapClient.java
@@ -1,10 +1,3 @@
-import java.net.URL;
-import java.util.ArrayList;
-
-import javax.xml.namespace.QName;
-import javax.xml.ws.Service;
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 
 import SoapInterface.SoapService;
 import SoapInterface.SoapServiceImpService;
@@ -13,7 +6,6 @@ public class SoapClient {
 	public static void main(String[] args) throws Exception{
 		SoapServiceImpService soapservice = new SoapServiceImpService();
 		SoapService soap = soapservice.getSoapServiceImpPort();
-//		ArrayList<Flight> ar = new Gson().fromJson(soap.getFlightlist(),new TypeToken<ArrayList<Flight>>(){}.getType());
 		try {
 			FlightSelectionforSoap frame = new FlightSelectionforSoap(soap);
 			frame.setVisible(true);
